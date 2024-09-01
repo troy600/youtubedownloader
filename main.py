983a729a23009ec7602e478051c5e8fd42a02a67
troy600 <bazartroy531@gmail.com>
2024-09-01 18:03:27 +0800

    Added the main.py

--- main.py
+++ main.py
@@ -17,20 +17,23 @@ for keycontext in os.listdir("/tmp"):
 
 #magic wahahahah
 print("type the youtube link you want to download. \n")
+
 link = input('>>')
+
+namemusi = input("name of the music\n >>")
+namemusic = f'"{namemusi}"'
+
 flink = f'{link}'
-
-
+print("Downloading the thumbnail")
 
 os.system(f"/usr/bin/yt-dlp '{flink}' --embed-thumbnail --no-download  -o /tmp/imafile")
+
+
 
 os.system(f'ffmpeg -i /tmp/imafile.webp -vf "crop=w=min(min(iw\,ih)\,720):h=min(min(iw\,ih)\,720),scale=720:720,setsar=1" -vframes 1 /tmp/21.png')
 
 os.system(f'yt-dlp -f140 "{flink}" --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -o /tmp/pske.m4a')
 os.system("ffmpeg -i /tmp/pske.m4a -c:a libmp3lame /tmp/pske.mp3")
-os.system("ffmpeg -i /tmp/pske.mp3 -i /tmp/21.png -map 1:0 -map 0:0 -c copy dissmusic.mp3")
+os.system(f"ffmpeg -i /tmp/pske.mp3 -i /tmp/21.png -map 1:0 -map 0:0 -c copy {namemusic}")
 
 print("success :)")
-
-#os.remove(f"/tmp/{delfile}")
-#os.remove(f"/tmp/{delfile2}")
