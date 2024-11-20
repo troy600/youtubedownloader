71e2492cff8fab083307272d1663f78088bbed2f
troy600 <bazartroy531@gmail.com>
2024-11-20 10:38:03 +0800

    main.py updates after very long time

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 import subprocess
+import asyncio
 
 delfile = '/tmp/21.png'
 delfile2 = "/tmp/imafile.webp"
 
-for keycontext in os.listdir("/tmp"):
-    if keycontext == '21.png':
-        os.remove(f"{delfile}")
-    elif keycontext == 'imafile.webp':
-        os.remove(delfile2)
-    elif keycontext == 'pske.m4a':
-        os.remove("/tmp/pske.m4a")
-    elif keycontext == "pske.mp3":
-        os.remove("/tmp/pske.mp3")
+async def removes():
+    for keycontext in os.listdir("/tmp"):
+        if keycontext == '21.png':
+            os.remove(f"{delfile}")
+        elif keycontext == 'imafile.webp':
+            os.remove(delfile2)
+        elif keycontext == 'pske.m4a':
+            os.remove("/tmp/pske.m4a")
+        elif keycontext == "pske.mp3":
+            os.remove("/tmp/pske.mp3")
 
 
 #magic wahahahah
@@ -26,14 +28,22 @@ namemusic = f'"{namemusi}"'
 flink = f'{link}'
 print("Downloading the thumbnail")
 
-os.system(f"/usr/bin/yt-dlp '{flink}' --embed-thumbnail --no-download  -o /tmp/imafile")
+async def download_thumbnail():
+    os.system(f"/usr/bin/yt-dlp '{flink}' --embed-thumbnail --no-download  -o /tmp/imafile &")
+
+async def download_m4a():
+    os.system(f'yt-dlp -f140 "{flink}" --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -o /tmp/pske.m4a')
+
+def ffmpeg_function():
+    os.system(f'ffmpeg -i /tmp/imafile.webp -vf "crop=w=min(min(iw\,ih)\,720):h=min(min(iw\,ih)\,720),scale=720:720,setsar=1" -vframes 1 /tmp/21.png')
+    os.system("ffmpeg -i /tmp/pske.m4a -c:a libmp3lame /tmp/pske.mp3")
+    os.system(f"ffmpeg -i /tmp/pske.mp3 -i /tmp/21.png -map 1:0 -map 0:0 -c copy {namemusic}")
+
+async def main():
+    await removes()
+    await asyncio.gather(download_thumbnail(), download_m4a())
 
 
-
-os.system(f'ffmpeg -i /tmp/imafile.webp -vf "crop=w=min(min(iw\,ih)\,720):h=min(min(iw\,ih)\,720),scale=720:720,setsar=1" -vframes 1 /tmp/21.png')
-
-os.system(f'yt-dlp -f140 "{flink}" --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -o /tmp/pske.m4a')
-os.system("ffmpeg -i /tmp/pske.m4a -c:a libmp3lame /tmp/pske.mp3")
-os.system(f"ffmpeg -i /tmp/pske.mp3 -i /tmp/21.png -map 1:0 -map 0:0 -c copy {namemusic}")
-
+asyncio.run(main())
+ffmpeg_function()
 print("success :)")
